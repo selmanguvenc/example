f2215bda63277f26452f344a1c1f25a45122e742
selmanguvenc <selmannguvenc@gmail.com>
2022-01-05 11:22:16 +0300

    str.go dosyası gönderildi

---
+++ str.go
@@ -0,0 +1,15 @@
+package main
+
+var degisken1 string
+var degisken2 int = 1
+var degisken3 string = "deger 3"
+
+func main() {
+
+	degisken1 = "deger 1"
+	degisken3 := "deger 4" // := değer atamak için kullanılır.
+
+	println(degisken1, degisken2, degisken3)
+	// main içinde var kullanmadan değişken tanımı yapılablir.
+
+}
